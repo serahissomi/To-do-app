7cc0415c32affdc99c45fb6e94bb764e679b6287
Dasom Lee <dsy20320@gmail.com>
2024-02-18 19:54:29 -0700

    새로운 todo 추가하기 버튼 눌렀을 때 리스트 아이템 추가 기능 추가

---
+++ script.js
@@ -0,0 +1,62 @@
+const list = document.getElementById('list');
+const createBtn = document.getElementById('create-btn');
+
+let todos = [];
+
+createBtn.addEventListener('click', createNewTodo);
+
+function createNewTodo() {
+    const item = {
+        id: new Date().getTime(),
+        text: '',
+        complete : false
+    }
+    todos.unshift(item);
+
+    const {itemEl, inputEl, editBtnEl, removeBtnEl} = createTodoElement(item);
+
+    list.prepend(itemEl);
+
+    inputEl.removeAttribute('disabled');
+
+    inputEl.focus();
+
+
+}
+
+function createTodoElement(item) {
+    const itemEl = document.createElement('div');
+    itemEl.classList.add('item');
+
+    const checkboxEl = document.createElement('input');
+    checkboxEl.type = 'checkbox';
+
+    if(item.complete) {
+        itemEl.classList.add('complete');
+    }
+
+    const inputEl = document.createElement('input');
+    inputEl.type = 'text';
+    inputEl.value = item.text;
+    inputEl.setAttribute('disabled', '');
+
+    const actionsEl = document.createElement('div');
+    actionsEl.classList.add('actions');
+
+    const editBtnEl = document.createElement('button');
+    editBtnEl.classList.add('material-icons');
+    editBtnEl.innerText = 'edit';
+
+    const removeBtnEl = document.createElement('button');
+    removeBtnEl.classList.add('material-icons', 'remove-btn');
+    removeBtnEl.innerText ='remove_circles';
+
+    actionsEl.append(editBtnEl);
+    actionsEl.append(removeBtnEl);
+
+    itemEl.append(checkboxEl);
+    itemEl.append(inputEl);
+    itemEl.append(actionsEl);
+
+    return {itemEl, inputEl, editBtnEl, removeBtnEl};
+}
