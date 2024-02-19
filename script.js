2504eae9b71b2ebdef2e92109fccd8721a95d691
Dasom Lee <dsy20320@gmail.com>
2024-02-18 20:14:28 -0700

    edit버튼과 remove 버튼 기능 추가

--- script.js
+++ script.js
@@ -51,6 +51,35 @@ function createTodoElement(item) {
     removeBtnEl.classList.add('material-icons', 'remove-btn');
     removeBtnEl.innerText ='remove_circles';
 
+
+    checkboxEl.addEventListener('change', () => {
+        item.complete = checkboxEl.checked;
+
+        if(item.complete) {
+            itemEl.classList.add('complete');
+        } else {
+            itemEl.classList.remove('complete');
+        }
+    })
+
+    inputEl.addEventListener('blur', () => {
+        inputEl.setAttribute('disabled', '');
+    })
+
+    inputEl.addEventListener('input', () => {
+        item.text = inputEl.value
+    })
+
+    editBtnEl.addEventListener('click', () => {
+        inputEl.removeAttribute('disabled');
+        inputEl.focus();
+    })
+
+    removeBtnEl.addEventListener('click', () => {
+        todos = todos.filter(t => t.id !== item.id);
+        itemEl.remove();
+    })
+
     actionsEl.append(editBtnEl);
     actionsEl.append(removeBtnEl);
 
